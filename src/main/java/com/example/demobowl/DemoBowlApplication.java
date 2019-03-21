9927883ea423395580a0475d8aefbfe1ba88011d
Tim Bennett <timbennett@corelogic.com>
2019-03-20 22:22:42 -0400

    first commit

---
+++ src/main/java/com/example/demobowl/DemoBowlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobowl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBowlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBowlApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demobowl/Game.java
@@ -0,0 +1,51 @@
+package com.example.demobowl;
+
+public class Game {
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
+
+    public void roll(int pins){
+        rolls[currentRoll++] = pins;
+    }
+
+    public int score(){
+        int score = 0;
+        int frameIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(frame)) {
+                score += 10 +
+                        strikeBonus(frameIndex);
+                frameIndex++;
+            } else if (isSpare(frameIndex)) {
+                score += 10 + spareBonus(frameIndex);
+                frameIndex += 2;
+            } else {
+                score += sumOfBallsInFrame(frameIndex);
+                frameIndex += 2;
+            }
+        }
+        return score;
+    }
+
+    private boolean isStrike(int frame) {
+        return rolls[frame] == 10;
+    }
+
+    private int strikeBonus(int frameIndex) {
+        return rolls[frameIndex + 1] +
+                spareBonus(frameIndex);
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return sumOfBallsInFrame(frameIndex) == 10;
+    }
+
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex + 2];
+    }
+
+    private int sumOfBallsInFrame(int frameIndex) {
+        return rolls[frameIndex]
+                + rolls[frameIndex + 1];
+    }
+}
